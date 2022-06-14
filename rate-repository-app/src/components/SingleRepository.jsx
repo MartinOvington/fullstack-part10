df4fc1948b25428cf6d13276e54be3cf29ba9133
Martin Ovington <MartinOvington@msn.com>
2022-06-14 08:58:37 +0100

    ex10.20 completed review list for single repository view

--- rate-repository-app/src/components/SingleRepository.jsx
+++ rate-repository-app/src/components/SingleRepository.jsx
@@ -1,13 +1,60 @@
 import { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { useParams } from 'react-router-native';
 import Text from './Text';
 import { GET_REPOSITORY } from '../graphql/queries';
 import RepositoryItem from './RepositoryItem';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+    backgroundColor: theme.colors.background,
+  },
+  container: {
+    margin: 5,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  reviewScore: {
+    margin: 7,
+    width: 35,
+    height: 35,
+    borderWidth: 2,
+    borderRadius: 25,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderColor: theme.colors.blueButton,
+    color: theme.colors.blueButton,
+  },
+  reviewText: {
+    paddingRight: 40,
+  },
+});
+
+const formatDate = (date) => {
+  const year = date.match(/\d{4}/);
+  const month = date.match(/-(\d{2})-/);
+  const day = date.match(/-(\d{2})T/);
+  return day[1] + '.' + month[1] + '.' + year;
+};
+//review.createdAt.replace(/T.*$/, '')
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const ReviewItem = ({ review }) => {
-  return <Text>Hello Mr. Thompson {review.text}</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.reviewScore}>{review.rating}</Text>
+      <View>
+        <Text fontSize="subheading" fontWeight="bold">
+          {review.user.username}
+        </Text>
+        <Text color="textSecondary">{formatDate(review.createdAt)}</Text>
+        <Text style={styles.reviewText}>{review.text}</Text>
+      </View>
+    </View>
+  );
 };
 
 const SingleRepository = () => {
@@ -23,7 +70,7 @@ const SingleRepository = () => {
     if (!loading && !error) {
       setRepository(data.repository);
     }
-  }, [loading]);
+  }, [loading, data]);
 
   const reviewNodes = repository
     ? repository.reviews.edges.map((edge) => edge.node)
@@ -35,7 +82,6 @@ const SingleRepository = () => {
     console.log(error);
     return null;
   } else if (!repository) {
-    console.log('no repo item');
     return null;
   }
   return (
@@ -44,8 +90,13 @@ const SingleRepository = () => {
       renderItem={({ item }) => <ReviewItem review={item} />}
       keyExtractor={({ id }) => id}
       ListHeaderComponent={() => (
-        <RepositoryItem repoItem={repository} urlButton />
+        <View>
+          <RepositoryItem repoItem={repository} urlButton />
+          <ItemSeparator />
+        </View>
       )}
+      ItemSeparatorComponent={ItemSeparator}
+      ListFooterComponent={ItemSeparator}
     />
   );
 };
