6ac03927ac116d19a56cc21b81e6019f08a57af0
Martin Ovington <MartinOvington@msn.com>
2022-06-17 09:59:22 +0100

    log out now navigates to root page

--- rate-repository-app/src/components/SignOutTab.jsx
+++ rate-repository-app/src/components/SignOutTab.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Pressable } from 'react-native';
+import { useNavigate } from 'react-router';
 import Text from './Text';
 import theme from '../theme';
 import { useApolloClient } from '@apollo/client';
@@ -9,12 +10,14 @@ const styles = StyleSheet.create({
 });
 
 const SignOutTab = () => {
+  const navigate = useNavigate();
   const apolloClient = useApolloClient();
   const authStorage = useAuthStorage();
 
   const signOut = () => {
     authStorage.removeAccessToken();
     apolloClient.resetStore();
+    navigate('/');
   };
 
   return (
